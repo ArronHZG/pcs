e4d8046e0d50529867b996463e933bf0f65b82e4
arron <hou.zg@foxmail.com>
2019-07-25 15:03:36 +0800

    Update system.sh

--- system.sh
+++ system.sh
@@ -2,4 +2,4 @@ docker stop system
 docker rm system
 docker rmi registry.cn-hangzhou.aliyuncs.com/xnt-pcs/pcs:system
 docker pull registry.cn-hangzhou.aliyuncs.com/xnt-pcs/pcs:system
-docker run -d -p 8005:8005 --name system registry.cn-hangzhou.aliyuncs.com/xnt-pcs/pcs:system
+docker run -d -p 8005:8005 --name system -v /etc/localtime:/etc/localtime:ro registry.cn-hangzhou.aliyuncs.com/xnt-pcs/pcs:system
