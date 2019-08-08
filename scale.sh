25f3b0b9fb26be06fa08f07ebdb3f6e3c890956f
ArronHZG <hou.zg@foxmail.com>
2019-08-09 00:52:05 +0800

    scale

--- scale.sh
+++ scale.sh
@@ -2,5 +2,5 @@ docker stop scale
 docker rm scale
 docker rmi registry.cn-hangzhou.aliyuncs.com/xnt-pcs/pcs:scale
 docker pull registry.cn-hangzhou.aliyuncs.com/xnt-pcs/pcs:scale
-docker run -d -p 8010:8010 --name scale -v /etc/localtime:/etc/localtime:ro registry.cn-hangzhou.aliyuncs.com/xnt-pcs/pcs:scale
+docker run -d -p 8007:8007 --name scale -v /etc/localtime:/etc/localtime:ro registry.cn-hangzhou.aliyuncs.com/xnt-pcs/pcs:scale
 # docker cp ~/pcs/simsun.ttc scale:/usr/share/fonts/
